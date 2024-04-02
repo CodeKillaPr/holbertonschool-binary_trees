e91dc95b2f5ca3ed6d52383ac3944194725772b4
KILLA <killa@MBP-de-KILLA.lan>
2024-04-02 16:32:29 -0400

    function that goes through a binary tree using post-order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,17 +2,17 @@
 
 /**
  * binary_tree_postorder - goes through a
- *binary tree using post-order traversal
+ * binary tree using post-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  * Return: void
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == null)
+	if (tree && func)
 	{
-		binary_tree_postorder(tree->left, func)
-			binary_tree_postorder(tree->right, func)
-				func(tree->n)
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
 	}
 }
