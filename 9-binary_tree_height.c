9aac3742152017ea3f713ca8c105976211c7b6fc
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-04-02 23:34:34 -0400

    adding code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,8 +17,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	return (binary_tree_height(tree->right) + 1);
-	return (binary_tree_height(tree->left) + 1);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (left_height + 1);
