c5e1d52ffbdabbfd0efc73493593fed2353e22a1
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-04-02 23:38:15 -0400

    fixing betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,12 +1,14 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depth - function that measures the depth of a node in a binary tree
+ * binary_tree_depth - function that measures
+ * the depth of a node in a binary tree
  * @tree: pointer to the node to measure the depth
  * Return: depth of the node
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
+
 	while (tree != NULL)
 	{
 		if (tree->parent != NULL)
