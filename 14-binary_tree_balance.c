b17427aa5df94ed2e3fa3f117bf8e6027ecc9c2f
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-04-03 17:04:02 -0400

    adding a addiotional function to this function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,24 @@
 #include "binary_trees.h"
 /**
+ * binary_trees_height - height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * @height: height of the
+ * Returns: height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
  * binary_tree_balance - function that measures
  * the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
@@ -13,9 +32,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		left_height = binary_tree_balance(tree->left) + 1;
+		left_height = binary_tree_height(tree->left);
 	if (tree->right)
-		right_height = binary_tree_balance(tree->right) + 1;
+		right_height = binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
