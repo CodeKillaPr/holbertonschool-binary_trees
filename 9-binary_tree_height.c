e9f4c5a0ad82e1923198e4ef96cfc387c27ff4d3
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-04-02 23:15:56 -0400

    fixing code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,6 +11,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
+	if (tree == NULL)
+		return (0);
+
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
