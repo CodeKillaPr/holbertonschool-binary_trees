26eb7ccb66a4820204d77d534bfaae842faeff49
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-04-04 14:35:42 -0400

    function that checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,7 +14,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+		return (1);
 
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
